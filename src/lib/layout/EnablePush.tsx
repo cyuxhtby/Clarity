abf206fb79b16e0ab2a16e463a680a500feed695
Diego Aguilar <d.aguilaro99@gmail.com>
2023-12-10 15:20:45 -0600

    remove button once enabled

--- src/lib/layout/EnablePush.tsx
+++ src/lib/layout/EnablePush.tsx
@@ -1,16 +1,29 @@
-import { IconButton, useColorMode } from '@chakra-ui/react';
-import { RiMoonFill, RiSunLine } from 'react-icons/ri';
-import { IoNotifications } from "react-icons/io5";
-import { IoMdNotificationsOff } from "react-icons/io";
-import { requestPermission } from '~/lib/utils/firebaseMessaging';
-
+import React, { useState, useEffect } from 'react';
+import { IconButton } from '@chakra-ui/react';
+import { IoNotifications } from 'react-icons/io5';
+import { requestPermission } from '~/lib/utils/firebaseMessaging'; 
 
 const EnablePush = () => {
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    setHasPermission(Notification.permission === 'granted');
+  }, []);
+
+  if (hasPermission) {
+    return null;
+  }
+
+  const handlePermissionRequest = async () => {
+    await requestPermission();
+    setHasPermission(Notification.permission === 'granted');
+  };
+
   return (
     <IconButton
-      aria-label="theme toggle"
+      aria-label="enable push notifications"
       icon={<IoNotifications />}
-      onClick={requestPermission}
+      onClick={handlePermissionRequest}
       mr={4}
     />
   );
