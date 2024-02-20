8301495b233942ed06e3ef2e8fe068057a00c157
Diego Aguilar <d.aguilaro99@gmail.com>
2024-02-20 17:22:23 -0600

    style: solid footer

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useColorModeValue } from '@chakra-ui/react';
 import SwipeIndicator from '~/lib/components/SwipeIndicator';
 import { useView } from '~/lib/contexts/ViewContext';
 import { useAuth } from '~/lib/contexts/AuthContext';
@@ -8,9 +8,10 @@ const Footer = () => {
   const { user } = useAuth();
   const currentIndex = viewOrder.indexOf(currentView);
   const total = viewOrder.length;
+  const bg = useColorModeValue('rgba(255, 255, 255, 1)', 'rgba(26, 32, 44, 1)');
 
   return (
-    <Flex as="footer" width="full" justifyContent="center" position="fixed" bottom="0" left="0" right="0" paddingBottom="4" marginTop="10" zIndex="10" backdropFilter="blur(30px)">
+    <Flex as="footer" width="full" justifyContent="center" position="fixed" bottom="0" left="0" right="0" paddingBottom="4" marginTop="10" zIndex="10" backgroundColor={bg}>
       {user && currentView !== 'signin' && <SwipeIndicator currentIndex={currentIndex} total={total} />}
       <Text fontSize="sm"></Text>
     </Flex>
