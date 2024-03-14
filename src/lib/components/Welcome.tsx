ede3320cdd9b021721499fac352dd6f10fd6a62c
Diego Aguilar <d.aguilaro99@gmail.com>
2024-03-13 21:30:21 -0500

    fix wed text width on mobile

--- src/lib/components/Welcome.tsx
+++ src/lib/components/Welcome.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { VStack, Text, Button, Box } from '@chakra-ui/react';
 import AddEvent from '~/lib/components/modals/AddEvent'
 import AddNote from './modals/AddNote';
+import { useBreakpointValue } from "@chakra-ui/react";
 
 export const Welcome = () => {
  
@@ -9,6 +10,7 @@ export const Welcome = () => {
     const day = now.toLocaleDateString('en-US', { weekday: 'long' });
     const date = now.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
     const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }).toLowerCase();
+    const dayFontSize = useBreakpointValue({ base: day.length > 8 ? "5xl" : "6xl", md: "6xl" }); // conditional for 'Wednesday' on mobile
 
 
   return (
@@ -22,7 +24,7 @@ export const Welcome = () => {
       zIndex="20"
     >
       <Box textAlign="center" mb={20} >
-        <Text fontSize="6xl" fontWeight="bold" mb={2}>
+        <Text fontSize={dayFontSize} fontWeight="bold" mb={2}>
           {day}
         </Text>
         <Text fontSize="2xl" fontWeight="bold">
