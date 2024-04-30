57b52040a4f37d81134070dc2836cc4b0f684872
Diego Aguilar <d.aguilaro99@gmail.com>
2024-04-30 00:14:52 -0500

    style: checklist

--- src/lib/components/Checklist.tsx
+++ src/lib/components/Checklist.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { VStack, Checkbox, Button, Input, useToast, Flex, Box } from '@chakra-ui/react';
+import { VStack, Checkbox, Button, Input, useToast, Flex, Box, useColorModeValue, Text} from '@chakra-ui/react';
 import { collection, addDoc, getDocs, deleteDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
 import { firestore as db } from '~/lib/utils/firebaseConfig';
 import { useAuth } from '~/lib/contexts/AuthContext';
 import AddTask from './modals/AddTask';
+import { color } from 'framer-motion';
 
 interface Task {
   id: string;
@@ -17,6 +18,8 @@ const Checklist = () => {
   const checklistRef = useRef<HTMLDivElement>(null);
   const { user } = useAuth();
   const toast = useToast();
+  // const bg = useColorModeValue('gray.50', 'blackAlpha.500');
+
 
   useEffect(() => {
     if (user) {
@@ -80,7 +83,7 @@ const Checklist = () => {
     <VStack
       ref={checklistRef}
       align="stretch"
-      spacing={2}
+      spacing={4}
       width="100%"
       overflowY="auto"
       minHeight={400}
@@ -88,15 +91,31 @@ const Checklist = () => {
       zIndex={20}
     >
       {tasks.map((task) => (
-       <Flex key={task.id} justify="center" width="100%">
-          <Box width="400px"  borderRadius="lg" padding="2" paddingLeft="8px" paddingRight="8px" backgroundColor={"blackAlpha.400"}>
-            <Checkbox
-              size="lg"
-              isChecked={task.completed}
-              onChange={() => removeTask(task)}
-            >
-              {task.title}
-            </Checkbox>
+        <Flex key={task.id} justify="center" width="100%">
+          <Box width="400px" borderWidth="1px" borderRadius="lg" padding="2" paddingLeft="12px" paddingRight="8px">
+            <Flex alignItems="center">
+              <Checkbox
+                size="lg"
+                isChecked={task.completed}
+                onChange={() => removeTask(task)}
+                sx={{
+                  '.chakra-checkbox__control': {
+                    borderColor: useColorModeValue('black', 'white'),
+                    _checked: {
+                      bg: useColorModeValue('black', 'white'),
+                      borderColor: useColorModeValue('black', 'white'),
+                      color: useColorModeValue('white', 'black'),
+                    },
+                    _hover: {
+                      bg: useColorModeValue('blackAlpha.400', 'whiteAlpha.400'),
+                    },
+                  },
+                }}
+              />
+              <Text ml={2} color={useColorModeValue('black', 'white')}>
+                {task.title}
+              </Text>
+            </Flex>
           </Box>
         </Flex>
       ))}
