da5be9427883c759fd71e72a97c2123c723adc94
Diego Aguilar <d.aguilaro99@gmail.com>
2024-04-29 22:02:46 -0500

    fix: footer styling cutoff

--- src/lib/components/ActivityPlanner.tsx
+++ src/lib/components/ActivityPlanner.tsx
@@ -1,92 +1,91 @@
 import React, { useState, useEffect } from 'react';
-import { VStack, useColorModeValue } from '@chakra-ui/react';
+import { VStack, useColorModeValue, Box } from '@chakra-ui/react';
 import { collection, addDoc, deleteDoc, doc, getDocs, setDoc } from "firebase/firestore";
 import { firestore as db } from '~/lib/utils/firebaseConfig';
 import { useAuth } from '~/lib/contexts/AuthContext';
-import HourBlock from './HourBlock'; 
+import HourBlock from './HourBlock';
 
 interface Activities {
   [hour: string]: string;
 }
 
 const ActivityPlanner: React.FC = () => {
-    const [activities, setActivities] = useState<Activities>({});
-    const { user } = useAuth();
-    const bg = useColorModeValue('gray.50', 'gray.700');
-  
-  
-      const fetchActivities = async () => {
-        if (!user) return;
-    
-        try {
-          console.log("Fetching activities for user:", user.uid);
-          const userDocRef = doc(db, "users", user.uid);
-          const activitiesCollectionRef = collection(userDocRef, "activities");
-          const activitiesSnapshot = await getDocs(activitiesCollectionRef);
-          const activitiesData: Activities = {};
-          activitiesSnapshot.forEach((docSnapshot) => {
-            activitiesData[docSnapshot.id] = docSnapshot.data().text;
-          });
-          setActivities(activitiesData);
-        } catch (error) {
-        }
-      };
-    
-      useEffect(() => {
-        fetchActivities();
-      }, [user]);
-    
-  
-      const saveActivity = async (hour: string, activityText: string) => {
-        if (!user || activityText.trim() === '') {
-            return;
-        }
-      
-        const userDocRef = doc(db, "users", user.uid);
-        const activityDocRef = doc(collection(userDocRef, "activities"), hour);
-      
-        try {
-          await setDoc(activityDocRef, { text: activityText });      
-          setActivities((prevActivities) => ({ ...prevActivities, [hour]: activityText }));
-        } catch (error: any) {
-          if ("code" in error && error.code === "permission-denied") {
-            console.error("Firestore permission denied:", error);
-          } else {
-            console.error("Error saving activity:", error);
-          }
-        }
-      };
+  const [activities, setActivities] = useState<Activities>({});
+  const { user } = useAuth();
+  const bg = useColorModeValue('gray.50', 'gray.700');
 
-      const deleteActivity = async (hour: string) => {
-        if (!user) {
-          console.error("No authenticated user found");
-          return;
-        }
-    
-        console.log("Deleting activity for hour:", hour);
-        const userDocRef = doc(db, "users", user.uid);
-        const activityDocRef = doc(collection(userDocRef, "activities"), hour);
-    
-        try {
-          await deleteDoc(activityDocRef);
-          console.log("Activity deleted successfully for hour:", hour);
-    
-          setActivities((prevActivities) => {
-            const updatedActivities = { ...prevActivities };
-            delete updatedActivities[hour];
-            return updatedActivities;
-          });
-        } catch (error: any) {
-          console.error("Error deleting activity:", error);
-        }
-      };
-      
-      
-  
-    const hours = Array.from({ length: 17 }, (_, i) => `${i + 6}:00`);
-  
-    return (
-        <VStack width="full" borderRadius="md" spacing={4} zIndex={20} position="relative">
+
+  const fetchActivities = async () => {
+    if (!user) return;
+
+    try {
+      console.log("Fetching activities for user:", user.uid);
+      const userDocRef = doc(db, "users", user.uid);
+      const activitiesCollectionRef = collection(userDocRef, "activities");
+      const activitiesSnapshot = await getDocs(activitiesCollectionRef);
+      const activitiesData: Activities = {};
+      activitiesSnapshot.forEach((docSnapshot) => {
+        activitiesData[docSnapshot.id] = docSnapshot.data().text;
+      });
+      setActivities(activitiesData);
+    } catch (error) {
+    }
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, [user]);
+
+
+  const saveActivity = async (hour: string, activityText: string) => {
+    if (!user || activityText.trim() === '') {
+      return;
+    }
+
+    const userDocRef = doc(db, "users", user.uid);
+    const activityDocRef = doc(collection(userDocRef, "activities"), hour);
+
+    try {
+      await setDoc(activityDocRef, { text: activityText });
+      setActivities((prevActivities) => ({ ...prevActivities, [hour]: activityText }));
+    } catch (error: any) {
+      if ("code" in error && error.code === "permission-denied") {
+        console.error("Firestore permission denied:", error);
+      } else {
+        console.error("Error saving activity:", error);
+      }
+    }
+  };
+
+  const deleteActivity = async (hour: string) => {
+    if (!user) {
+      console.error("No authenticated user found");
+      return;
+    }
+
+    console.log("Deleting activity for hour:", hour);
+    const userDocRef = doc(db, "users", user.uid);
+    const activityDocRef = doc(collection(userDocRef, "activities"), hour);
+
+    try {
+      await deleteDoc(activityDocRef);
+      console.log("Activity deleted successfully for hour:", hour);
+
+      setActivities((prevActivities) => {
+        const updatedActivities = { ...prevActivities };
+        delete updatedActivities[hour];
+        return updatedActivities;
+      });
+    } catch (error: any) {
+      console.error("Error deleting activity:", error);
+    }
+  };
+
+  const hours = Array.from({ length: 17 }, (_, i) => `${i + 6}:00`);
+
+  return (
+    <>
+      <VStack width="full" borderRadius="md" spacing={4} zIndex={20} position="relative">
         {hours.map((hour, index) => (
           <HourBlock
             key={index}
@@ -96,8 +95,11 @@ const ActivityPlanner: React.FC = () => {
             activity={activities[hour]}
           />
         ))}
+        {/* spacing to not get cut off by footer */}
+        <Box height={10} />
       </VStack>
-    );
-  };
-  
-  export default ActivityPlanner;
+    </>
+  );
+};
+
+export default ActivityPlanner;
